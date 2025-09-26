b9a41cde7253921af60a7e4c21b75e34b1a051ce
q4niel <github.obscure938@passmail.net>
2025-09-26 21:27:31 +0200

    add Dockerfile.linux & docker_util.sh

---
+++ env/run/linux/util/docker_util.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+container="dewfall"
+
+runDocker() {
+    local dockerfile=$1
+    local pyScript=$2
+    local pyArgs=("${@:3}")
+
+    # Remove container if it already exists
+    if [ "$(sudo docker ps -a -q -f name=^${container})" ]; then
+        sudo docker rm -f "$container"
+    fi
+
+    # Build image
+    sudo docker build -f "$dockerfile" -t "$container:latest" .
+
+    # Run container
+    sudo docker run --name "$container" "$container:latest" "$pyScript" "${pyArgs[@]}"
+}
+
+copyDocker() {
+    local dockerPath=$1
+    local outPath=$2
+
+    # Copy from container to local
+    sudo docker cp "$container:/$dockerPath" "./$outPath"
+
+    # Make current user own $outPath
+    sudo chown -R $(whoami):$(whoami) "./$outPath"
+}
