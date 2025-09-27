93e8cc7387334c330b4921a4a895a866596037cd
q4niel <github.obscure938@passmail.net>
2025-09-27 04:27:18 +0200

    rename src/build.toml -> src/config.toml

--- env/src/build.py
+++ env/src/build.py
@@ -2,20 +2,20 @@ import tomllib
 from util import clang
 
 if __name__ == "__main__":
-    with open("src/build.toml", "rb") as file:
-        buildData: dict = tomllib.load(file)
+    with open("src/config.toml", "rb") as file:
+        configData: dict = tomllib.load(file)
 
     clang.init()
 
     shallLink: bool = True
-    for src in buildData["global"]["compile"]:
+    for src in configData["global"]["compile"]:
         if not clang.compile (
             f"src/{src}",
-            buildData["flags"]["shared"] + buildData["flags"]["compile"]
+            configData["flags"]["shared"] + configData["flags"]["compile"]
         ): shallLink = False
 
     print()
     clang.linkExec (
         "app",
-        buildData["flags"]["shared"] + buildData["flags"]["link"]
+        configData["flags"]["shared"] + configData["flags"]["link"]
     )
