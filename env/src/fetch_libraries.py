71d7ac28d17ce17e5300e9f90a4d58816cc540c0
q4niel <github.obscure938@passmail.net>
2025-10-01 18:24:33 +0200

    add 3rd party library renaming

--- env/src/fetch_libraries.py
+++ env/src/fetch_libraries.py
@@ -3,22 +3,33 @@ import shutil
 import tarfile
 import requests
 import tomllib
+from util.platform import Platform
 
-def fetch(path: str, url: str) -> None:
-    with open(f"3rd/raw/{path}", "wb") as file:
+def fetch(name: str, url: str) -> None:
+    preEntries: list[str] = os.listdir("3rd")
+    archive: str = url.replace("/", "")
+
+    with open(f"3rd/raw/{archive}", "wb") as file:
         file.write(requests.get(url).content)
 
-    with tarfile.open(f"3rd/raw/{path}", "r:*") as tar:
-        tar.extractall(path=f"3rd/{path}")
+    with tarfile.open(f"3rd/raw/{archive}", "r:*") as tar:
+        tar.extractall(path=f"3rd")
+
+    for entry in os.listdir("3rd"):
+        if entry in preEntries: continue
+        os.rename(f"3rd/{entry}", f"3rd/{name}")
 
 if __name__ == "__main__":
+    if os.path.exists("3rd"):
+        shutil.rmtree("3rd")
+
     os.mkdir("3rd")
     os.mkdir("3rd/raw")
 
     with open("src/build.toml", "rb") as file:
         data: dict = tomllib.load(file)
 
-    for lib in data["global"]["fetchLibraries"]:
-        fetch(lib["path"], lib["url"])
+    for lib in data["global"]["3rdLibraries"][Platform.getString()]:
+        fetch(lib["name"], lib["url"])
 
     shutil.rmtree("3rd/raw")
